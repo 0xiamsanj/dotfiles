1e8e14666836b3ce13cdd5ba03334c26af43faec
0xiamsanj <sanjai04022002@gmail.com>
2024-03-25 22:37:50 +0530

    Neovim submodule removed

---
+++ .config/nvim/after/plugin/harpoon.lua
@@ -0,0 +1,10 @@
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-g>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-i>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end)
