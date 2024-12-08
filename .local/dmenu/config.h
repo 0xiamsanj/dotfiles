9c05ba7b34c9d91a5291182ac5b03a4aa80ef5e6
0xiamsanj <sanjai04022002@gmail.com>
2024-12-08 23:05:46 +0530

    [dmenu] rosepine colorscheme

--- .local/dmenu/config.h
+++ .local/dmenu/config.h
@@ -10,8 +10,14 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
+
+    // Nord color scheme
 	[SchemeSel] = { "#000000", "#a1baf0" },
 	[SchemeOut] = { "#a1baf0", "#ebcb8b" },
+
+    // Rosepine color scheme
+	[SchemeOut] = { "#d7828e", "#ebcb8b" },
+	[SchemeSel] = { "#000000", "#d7828e" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
